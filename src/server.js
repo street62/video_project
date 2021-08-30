5beeecedb192e63e1bd72448f52cd5f5f57f7e76
street62 <tmdfl1027@gmail.com>
2021-08-30 14:09:42 +0900

    profile image 경로수정 + file static 추가

--- src/server.js
+++ src/server.js
@@ -27,6 +27,7 @@ app.use(session({
 app.use(localsMiddleware)
 
 app.use("/", rootRouter)
+app.use("/uploads", express.static("uploads"))
 app.use("/videos", videoRouter)
 app.use("/users", userRouter)
 
